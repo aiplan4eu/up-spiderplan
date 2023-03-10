73e0ee5cd030702bfcf5e6df64888574adde9d4b
Uwe Köckemann <uwe.kockemann@oru.se>
2023-03-10 14:25:59 +0100

    Added matplotlib depdendency, path plotting function

--- up_spiderplan/test.py
+++ up_spiderplan/test.py
@@ -1,15 +1,8 @@
 from unified_planning.test.examples import get_example_problems
 from combined_task_motion_planning.office.test_01 import TampOffice01
-
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import matplotlib.patches as patches
-from PIL import Image
-
-import math
-
 from solver import EngineImpl
+
+from util import plot_path
 
 problems = get_example_problems()
 
@@ -32,42 +25,4 @@ selected = office_tamp
 solver = EngineImpl()
 result = solver.solve(selected)
 
-plotted = []
-ax = None
-fix = None
-
-for a in result.plan.actions:
-    print(a)
-    if a.motion_paths is not None:
-        for key in a.motion_paths:
-            # print(f"  {key} -> {a.motion_paths[key]}")
-            # print(f"  Map: {key.starting.type.occupancy_map}")
-            map_file = key.starting.type.occupancy_map.filename
-            map_path = map_file.replace(map_file.split("/")[-1], "")
-            path = a.motion_paths[key]
-            image_file = None
-            resolution = 1.0
-            f = open(map_file)
-            for l in f.readlines():
-                if "image: " in l:
-                    image_file = map_path + "/" + l.replace("image: ", "").strip()
-                if "resolution: " in l:
-                    resolution = float(l.replace("resolution: ", "").strip())
-            if image_file is not None:
-                if image_file not in plotted:
-                    im = Image.open(image_file)
-                    fig, ax = plt.subplots()
-                    ax.imshow(im)
-                    plotted.append(image_file)
-
-                for step in path:
-                    x = step[0][0]/resolution
-                    y = step[0][1]/resolution
-                    w = step[0][2]
-                    x_d = math.sin(w)
-                    y_d = math.cos(w)
-                    plt.arrow(x, im.height - y, x_d, y_d)
-plt.show()
-plt.waitforbuttonpress()
-
-
+plot_motion(result)
---
+++ up_spiderplan/util.py
@@ -0,0 +1,41 @@
+import math
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+def plot_path(result):
+    plotted = []
+    ax = None
+    fix = None
+
+    for a in result.plan.actions:
+        if a.motion_paths is not None:
+            for key in a.motion_paths:
+                # print(f"  {key} -> {a.motion_paths[key]}")
+                # print(f"  Map: {key.starting.type.occupancy_map}")
+                map_file = key.starting.type.occupancy_map.filename
+                map_path = map_file.replace(map_file.split("/")[-1], "")
+                path = a.motion_paths[key]
+                image_file = None
+                resolution = 1.0
+                f = open(map_file)
+                for l in f.readlines():
+                    if "image: " in l:
+                        image_file = map_path + "/" + l.replace("image: ", "").strip()
+                    if "resolution: " in l:
+                        resolution = float(l.replace("resolution: ", "").strip())
+                if image_file is not None:
+                    if image_file not in plotted:
+                        im = Image.open(image_file)
+                        fig, ax = plt.subplots()
+                        ax.imshow(im)
+                        plotted.append(image_file)
+
+                    for step in path:
+                        x = step[0][0]/resolution
+                        y = step[0][1]/resolution
+                        w = step[0][2]
+                        x_d = math.cos(w)
+                        y_d = math.sin(w)
+                        plt.arrow(x, im.height - y, x_d, y_d)
+    plt.show()
