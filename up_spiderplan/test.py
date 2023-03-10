bc28b5804d453f161da29b376c930f416b3b9147
Uwe Köckemann <uwe.kockemann@oru.se>
2023-03-10 15:58:16 +0100

    More test cases

--- up_spiderplan/test.py
+++ up_spiderplan/test.py
@@ -1,5 +1,8 @@
 from unified_planning.test.examples import get_example_problems
+import unified_planning.engines.results as results
 from combined_task_motion_planning.office.test_01 import TampOffice01
+from combined_task_motion_planning.office.test_02 import TampOffice02
+from combined_task_motion_planning.office.test_03 import TampOffice03
 from solver import EngineImpl
 
 from util import plot_path
@@ -14,15 +17,20 @@ counter_2_50 = problems["counter_to_50"].problem
 match_cellar = problems["matchcellar"].problem
 robot_no_neg = problems["robot_no_negative_preconditions"].problem
 
-tamp = problems["tamp_feasible"].problem
+# tamp = problems["tamp_feasible"].problem
 
-office_tamp = TampOffice01(1).get_problem()
+office_tamp_01 = TampOffice01(1).get_problem()
+office_tamp_02 = TampOffice02(1).get_problem()
+office_tamp_03 = TampOffice03(1).get_problem()
 
-selected = office_tamp
+selected = office_tamp_03
 
 # print(selected)
 
 solver = EngineImpl()
 result = solver.solve(selected)
 
-plot_motion(result)
+if result.status in results.POSITIVE_OUTCOMES:
+    plot_path(result)
+else:
+    print("NO-PLAN-FOUND")
